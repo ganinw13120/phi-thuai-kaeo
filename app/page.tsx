13edd14880fca785f931e299d21dd59135a5af4a
ganinw13120 <gan131201@gmail.com>
2023-11-04 22:27:19 +0700

    [.] init project and structures, ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,145 @@
-import Image from 'next/image'
+'use client'
+import { createRef, RefObject, useEffect, useRef, useState } from "react"
+import Image from "@/node_modules/next/image";
+import Hand from '../public/hand.png'
+import { Avatar, Button, Divider, Input, List, ListItem, ListItemDecorator, Typography } from "@/node_modules/@mui/joy/index";
+
+type Position = {
+  x: number
+  y: number
+}
+
+type Person = {
+  name: string
+  objRef: any
+}
 
 export default function Home() {
+
+  const [names, setNames] = useState<Person[]>([]);
+  const [name, setName] = useState<string>("");
+
+  const startRef = useRef<HTMLDivElement>(null)
+  const handRef = useRef<HTMLImageElement>(null)
+
+  const [handPosition, setHandPosition] = useState<Position>({
+    x: 200,
+    y: 200
+  })
+
+  const random = () => {
+
+  }
+
+  const calculatePosition = (boxRef: any): Position => {
+    const box = (startRef.current! as any).getBoundingClientRect()
+    const hand = (handRef.current! as any).getBoundingClientRect()
+    return {
+      x: (box.x + (box.width / 2)) - (hand.width / 2),
+      y: (box.y + (box.height / 2)) - (hand.height / 2),
+    }
+  }
+
+  useEffect(() => {
+    reset()
+  }, [startRef.current, names])
+
+  const reset = () => {
+    if (!startRef) return
+    setHandPosition(calculatePosition(startRef))
+  }
+
+  const addName = () => {
+    if (!name) return
+    const newRef = createRef()
+    setNames(prev => {
+      let tmp = [...prev]
+      tmp.push({
+        name: name,
+        objRef: newRef
+      })
+      return tmp
+    })
+    setName("")
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="relative z-0">
+        <Image src={Hand} alt="" ref={handRef} className="absolute hand" style={{ transform: `translate(${handPosition.x}px, ${handPosition.y}px)` }} />
+      </div>
+      <main className="min-h-screen px-20 py-6">
+        <div className="m-2 z-1 relative">
+          <div className="flex gap-3">
+            <Button className="" size="lg" variant="solid" color="primary" onClick={random}>
+              Random
+            </Button>
+            <Button className="" size="lg" variant="outlined" color="primary" onClick={reset}>
+              Reset
+            </Button>
+            <input placeholder="x" onChange={(e) => {
+              setHandPosition(prev => {
+                return {
+                  x: parseFloat(e.target.value),
+                  y: prev.y,
+                }
+              })
+            }} />
+            <input placeholder="y" onChange={(e) => {
+              setHandPosition(prev => {
+                return {
+                  y: parseFloat(e.target.value),
+                  x: prev.x,
+                }
+              })
+            }} />
+          </div>
+          <div className="flex my-2 gap-3">
+            <Typography className="my-auto">Add new person : </Typography>
+            <Input variant="soft" placeholder="name..." value={name} onChange={(e: any) => {
+              setName(e.target.value)
+            }} />
+            <Button className="" size="lg" variant="solid" color="primary" onClick={addName}>
+              Add
+            </Button>
+          </div>
+          <div className="grid grid-cols-3 my-2">
+            {
+              names.map(n => {
+                return (
+                  <div className="flex gap-3 mr-2 my-2 bg-white p-2">
+                    <Avatar>{n.name[0]}</Avatar>
+                    <Typography className="my-auto">{n.name}</Typography>
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
-      </div>
+        <Divider />
+        <div className="mt-8 grid grid-cols-4 grid-rows-3">
+          <div className="cell col-span-2 font-bold" ref={startRef}>
+          </div>
+          {
+            names.map((person, k) => {
+              return <Cell text={person.name} objRef={person.objRef} key={k} />
+            })
+          }
+        </div>
+      </main></>
+  )
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+interface CellProps {
+  text: string
+  objRef: RefObject<HTMLDivElement>
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+const Cell: React.FC<CellProps> = (props: CellProps) => {
+  return (
+    <div className="cell" ref={props.objRef}>
+      {props.text}
+    </div>
   )
 }
